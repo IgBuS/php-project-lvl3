5793c85bfd02b486944a33f3028a5561d1a4da4d
Buzin Igor <i.b@msulab.ru>
2020-11-11 15:07:12 +0300

    add view

---
+++ resources/views/main.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="9Fz0U5g6uKm3MNp5gZNYiBjQ3GtY9xUQPkCATv1x">
+
+        <title>Laravel</title>
+
+        <!-- Scripts -->
+        <script src="https://php-l3-page-analyzer.herokuapp.com/js/app.js" defer></script>
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="https://php-l3-page-analyzer.herokuapp.com/css/app.css" rel="stylesheet">
+    </head>
+    <body class="d-flex flex-column">
+        <header>
+            <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+                <a class="navbar-brand" href="https://php-l3-page-analyzer.herokuapp.com">Analyzer</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="https://php-l3-page-analyzer.herokuapp.com">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="https://php-l3-page-analyzer.herokuapp.com/domains">Domains</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+
+        <main class="flex-grow-1">
+                            <div class="jumbotron jumbotron-fluid bg-dark">
+        <div class="container-lg">
+            <div class="row">
+                <div class="col-12 col-md-10 col-lg-8 mx-auto text-white">
+                    <h1 class="display-3">Page Analyzer</h1>
+                    <p class="lead">Check web pages for free</p>
+                    <form action="https://php-l3-page-analyzer.herokuapp.com/domains" method="post" class="d-flex justify-content-center">
+                        <input type="hidden" name="_token" value="9Fz0U5g6uKm3MNp5gZNYiBjQ3GtY9xUQPkCATv1x">                        <input type="text" name="domain[name]" value="" class="form-control form-control-lg" placeholder="https://www.example.com">
+                        <button type="submit" class="btn btn-lg btn-primary ml-3 px-5 text-uppercase">Check</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+        </main>
+
+        <footer class="border-top py-3 mt-5">
+            <div class="container-lg">
+                <div class="text-center">
+                    created by
+                    <a href="https://hexlet.io/pages/about" target="_blank">Hexlet</a>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
