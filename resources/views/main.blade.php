9316d30edc6b666aa4e45efee5b664ca37be8e5d
Buzin Igor <i.b@msulab.ru>
2020-11-27 16:03:17 +0300

    step

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -27,7 +27,7 @@
                 <div class="col-12 col-md-10 col-lg-8 mx-auto text-white">
                     <h1 class="display-3">Page Analyzer</h1>
                     <p class="lead">Check web pages for free</p>
-                    <form action="http://homestead.local/domains" method="post" class="d-flex justify-content-center">
+                    <form action="https://pacific-temple-99321.herokuapp.com/domains" method="post" class="d-flex justify-content-center">
                         {{csrf_field()}}
                             <input type="text" name="domain[name]" value="" class="form-control form-control-lg" placeholder="https://www.example.com">
                         <button type="submit" class="btn btn-lg btn-primary ml-3 px-5 text-uppercase">Check</button>
