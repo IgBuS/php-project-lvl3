e0005439ca23b044406dccc963dd0d0e53cb9dc3
IgBuS <i.b@msulab.ru>
2022-02-28 16:59:54 +0300

    fix

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -92,9 +92,9 @@ class UrlController extends Controller
             flash('Url created successfuly')->success();
             return redirect()->route('urls.show', ['url' => $id]);
         } else {
-            $id = DB::table('urls')->where('name', $normalizedUrl)->first()->id;
             flash('URL already exists')->warning();
-            if ($id) {
+            if (DB::table('urls')->where('name', $normalizedUrl)->first()->id) {
+                $id = DB::table('urls')->where('name', $normalizedUrl)->first()->id;
                 return redirect()->route('urls.edit', ['url' => $id]);
             } else {
                 return redirect()->route('main');
