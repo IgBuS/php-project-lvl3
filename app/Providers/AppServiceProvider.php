926d146940784b935ce7332a39149df3a8d70a27
IgBuS <i.b@msulab.ru>
2022-11-16 11:46:47 +0300

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
         if (env('APP_ENV') !== 'local') {
-            \URL::forceScheme('https');
+            URL::forceScheme('https');
         }
     }
 }
